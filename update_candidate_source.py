74ad271b133c3f4ade68c48dddf828ddf5ad9923
Ashish Paliwal <ashishhpaliwal@gmail.com>
2023-10-24 10:37:35 +0530

    changing source of all candidates in db

--- update_candidate_source.py
+++ update_candidate_source.py
@@ -1,10 +1,17 @@
-from candidates.models import Candidate
+from django.core.management.base import BaseCommand
+from candidates.models import Candidate  # Replace 'your_app' with your actual app name
 
-# Get all candidates with source='application'
-candidates_to_update = Candidate.objects.filter(source='application')
+class Command(BaseCommand):
+    help = 'Update source field for all candidates from "application" to "recruiter"'
 
-# Update the source for each candidate and save it
-for candidate in candidates_to_update:
-    candidate.source = 'recruiter'
-    candidate.save()
+    def handle(self, *args, **options):
+        # Get all candidates with source='application'
+        candidates_to_update = Candidate.objects.filter(source='application')
+
+        # Update the source for each candidate and save it
+        for candidate in candidates_to_update:
+            candidate.source = 'recruiter'
+            candidate.save()
+
+        self.stdout.write(self.style.SUCCESS('Successfully updated source for candidates.'))
 
