5287dd9aa207edff62bf2dfd66799d38607097e7
Ashish Paliwal <ashishhpaliwal@gmail.com>
2023-10-24 10:26:49 +0530

    job application redirect from careers page

--- jobportal/views.py
+++ jobportal/views.py
@@ -101,3 +101,17 @@ def job_detail_and_apply(request, job_id):
 
 
 
+# your_app/management/commands/change_candidate_source.py
+
+from django.core.management.base import BaseCommand
+from candidates import Candidate  # Update with your actual app name
+
+class Command(BaseCommand):
+    help = 'Change the source field of all candidates to "recruiter"'
+
+    def handle(self, *args, **kwargs):
+        try:
+            Candidate.objects.update(source='recruiter')
+            self.stdout.write(self.style.SUCCESS('Successfully updated source field to "recruiter" for all candidates.'))
+        except Exception as e:
+            self.stderr.write(self.style.ERROR(f'An error occurred: {str(e)}'))
